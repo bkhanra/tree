3caab024e0f86ec8f545c55dbc82ae9c40f7cd6a
bkhanra <biswajitkhanra@gmail.com>
2026-04-07 19:00:17 +0530

    today's update

--- TreeNode.java
+++ TreeNode.java
@@ -6,11 +6,15 @@ public class TreeNode<T> {
 	
 	T data;
 	ArrayList<TreeNode<T>> children;
+
 	
 	public TreeNode(T data){
 		this.data = data;
 		this.children = new ArrayList<>();
+	
 	}
+	
+	
 	
 
 }
--- TreeUseCase.java
+++ TreeUseCase.java
@@ -1,12 +1,20 @@
 package trees;
 
 public class TreeUseCase {
+	
+	public static void printTree(TreeNode<Integer> root) {
+		System.out.print(root.data + " ");
+		for(int i = 0; i < root.children.size(); i++) {
+			TreeNode<Integer> child = root.children.get(i);
+			printTree(child);
+		}
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		TreeNode<Integer> root = new TreeNode<>(4);
-		System.out.println(root.data);
+		
 		TreeNode<Integer> child1 = new TreeNode<>(2);
 		TreeNode<Integer> child2 = new TreeNode<>(3);
 		TreeNode<Integer> child3 = new TreeNode<>(1);
@@ -18,7 +26,9 @@ public class TreeUseCase {
 		child2.children.add(child4);
 		child2.children.add(child5);
 		
+		printTree(root);
 		
+	
 		
 		
 
